04e6883e25ecf8a7aec7a8974cf1b616ddc5f5f8
Tefa Zapata <tefaz.n@gmail.com>
2019-09-10 13:12:47 -0500

    smile-music

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+
+  artistas: any[] = [];
+  loading: boolean;
+  
+  constructor(private spotify:SpotifyService) { }
+
+  buscar(termino: string) {
+    console.log(termino);
+
+    this.loading = true;
+    this.spotify.getArtistas( termino )
+    .subscribe( (data: any) => {
+      console.log(data);
+      this.artistas = data;
+      this.loading = false;
+    });
+  }
+
+  
+
+}
